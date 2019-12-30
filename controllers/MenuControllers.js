6528848549300d91229889721e1af5b1b5441d3b
Joseph <joseph@Josephs-MacBook-Pro.local>
2019-12-30 16:00:37 -0500

    implemented contact listing feature and search methods

--- controllers/MenuControllers.js
+++ controllers/MenuControllers.js
@@ -9,6 +9,8 @@ module.exports = class MenuControllers {
           message: "Please choose from an option below: ",
           choices: [
             "Add new contact",
+            "View all contacts:",
+            "Search for a contact",
             "Exit"
           ]
         }
@@ -29,6 +31,12 @@ main() {
         default:
           console.log("Invalid input");
           this.main();
+          case "view all contacts": 
+          this.getContacts();
+          break
+          case "Search for a contact":
+      this.search();
+      break;
       }
     })
     .catch((err) => {
@@ -42,7 +50,7 @@ clear() {
 addContact(){
     this.clear();
     inquirer.prompt(this.book.addContactQuestions).then((answers) => {
-      this.book.addContact(answers.name, answers.phone).then((contact) => {
+      this.book.addContact(answers.name, answers.phone, answers.email).then((contact) => {
         console.log("Contact added successfully!");
         this.main();
       }).catch((err) => {
@@ -57,11 +65,98 @@ addContact(){
     console.log("Thanks for using AddressBloc!");
     process.exit();
   }
+  search(){
+    inquirer.prompt(this.book.searchQuestions)
+    .then((target) => {
+     this.book.search(target.name)
+     .then((contact) => {
+        if(contact === null){
+          this.clear();
+          console.log("contact not found");
+          this.search();
+        } else {
+          this.showContact(contact);
+       }
+
+      });
+   })
+   .catch((err) => {
+     console.log(err);
+     this.main();
+   });
+  }
+
+  showContact(contact){
+    this._printContact(contact);
+    inquirer.prompt(this.book.showContactQuestions)
+    .then((answer) => {
+      switch(answer.selected){
+        case "Delete contact":
+          this.delete(contact);
+          break;
+        case "Main menu":
+          this.main();
+          break;
+        default:
+          console.log("Something went wrong.");
+          this.showContact(contact);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      this.showContact(contact);
+    });
+  }
+ 
+    delete(contact) {
+      inquirer.prompt(this.book.deleteConfirmQuestions)
+      .then((answer) => {
+        if(answer.confirmation){
+          this.book.delete(contact.id);
+          console.log("contact deleted!");
+          this.main();
+        } else {
+          console.log("contact not deleted");
+          this.showContact(contact);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        this.main();
+      });
+    }
+
+  _printContact(contact){
+    console.log(`
+      name: ${contact.name}
+      phone number: ${contact.phone}
+      email: ${contact.email}
+      ---------------`
+    );
+  }
 
 getContactCount() {
 return this.contacts.length;
 }
 remindMe() {
   return ('Learning is a life-long pursuit')
+}
+getContacts() {
+  this.clear(); 
+
+  this.book.getContacts().then((contacts) => {
+    for (let contact of contacts) {
+      console.log(`
+      name: ${contact.name}
+      phone number: ${contact.phone}
+      email: ${contact.email}
+      ---------------`
+      );
+    }
+    this.main();
+  }).catch((err) => {
+    console.log(err);
+    this.main();
+  });
 }
 }
